d7e7000f970502da4bf62e9572bc92efd43b7e64
Matheus <mgusmao.dev@gmail.com>
2025-04-19 02:27:47 -0300

    chore: init

---
+++ src/main/java/br/com/gusmaomatheus/car_repair/CarRepairApplication.java
@@ -0,0 +1,13 @@
+package br.com.gusmaomatheus.car_repair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRepairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRepairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gusmaomatheus/car_repair/CarRepairApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gusmaomatheus.car_repair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRepairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
