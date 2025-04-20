230f13209249a4eefd2e4b2dce479e305037dbc9
Matheus <mgusmao.dev@gmail.com>
2025-04-20 15:58:02 -0300

    fix(migrate): corrigir migrate

--- src/main/resources/db/migration/V7__alterar_coluna_anos_de_experiencia.sql
+++ src/main/resources/db/migration/V7__alterar_coluna_anos_de_experiencia.sql
@@ -1,2 +1,17 @@
--- Remover constraint not null (campo não é obrigatório pelo modelo)
-ALTER TABLE IF EXISTS consertos ALTER COLUMN anos_de_experiencia INTEGER;
+-- Remover NOT NULL de colunas não obrigatórias
+
+DROP TABLE IF EXISTS consertos;
+
+CREATE TABLE IF NOT EXISTS consertos (
+    id INTEGER AUTO_INCREMENT,
+    data_entrada DATE NOT NULL,
+    data_saida DATE NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    anos_de_experiencia INTEGER,
+    marca  VARCHAR(255) NOT NULL,
+    modelo VARCHAR(255) NOT NULL,
+    ano    VARCHAR(255) NOT NULL,
+    cor VARCHAR(255),
+    ativo BOOLEAN DEFAULT TRUE,
+    CONSTRAINT pk_consertos PRIMARY KEY (id)
+);
