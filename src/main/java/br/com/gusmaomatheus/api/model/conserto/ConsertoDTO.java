3b02da3b6071a686094cb121086a668ffc6c9449
Matheus <mgusmao.dev@gmail.com>
2025-04-20 03:19:14 -0300

    fix(model/dto): validações aninhadas não funcionando

--- src/main/java/br/com/gusmaomatheus/api/model/conserto/ConsertoDTO.java
+++ src/main/java/br/com/gusmaomatheus/api/model/conserto/ConsertoDTO.java
@@ -3,7 +3,9 @@ package br.com.gusmaomatheus.api.model.conserto;
 import br.com.gusmaomatheus.api.model.mecanico.MecanicoDTO;
 import br.com.gusmaomatheus.api.model.veiculo.VeiculoDTO;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record ConsertoDTO(
@@ -13,6 +15,10 @@ public record ConsertoDTO(
         @NotBlank(message = "O campo 'data_saida' é obrigatório.")
         @Pattern(regexp = "^\\d{2}/\\d{2}/\\d{4}$\n", message = "O campo 'data_saida' deve seguir o padrão: dd/mm/yyyy (Ex.: 20/04/2025)")
         String dataSaida,
+        @NotNull
+        @Valid
         MecanicoDTO dadosMecanico,
+        @NotNull
+        @Valid
         VeiculoDTO dadosVeiculo
 ) {}
