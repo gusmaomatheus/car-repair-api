c744b370b9d18cb1a39fba9068252d66b641d54c
Matheus <mgusmao.dev@gmail.com>
2025-04-20 05:07:48 -0300

    feat(Mecanico): alterar campos obrigatórios dos dados do mecanico

--- src/main/java/br/com/gusmaomatheus/api/model/mecanico/MecanicoDTO.java
+++ src/main/java/br/com/gusmaomatheus/api/model/mecanico/MecanicoDTO.java
@@ -1,13 +1,9 @@
 package br.com.gusmaomatheus.api.model.mecanico;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
 
 public record MecanicoDTO(
         @NotBlank(message = "O campo 'nome' é obrigatório")
         String nome,
-        @NotNull(message = "O campo 'anosDeExperiencia' é obrigatório")
-        @PositiveOrZero(message = "O campo 'anosDeExperiencia' deve ser igual ou maior do que zero.")
         Integer anosDeExperiencia
 ) {}
