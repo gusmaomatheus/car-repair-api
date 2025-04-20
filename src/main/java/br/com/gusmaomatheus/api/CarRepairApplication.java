5fc3d707aae7b7ec77dd5aa8b17c2744056686c2
Matheus <mgusmao.dev@gmail.com>
2025-04-20 04:03:15 -0300

    feat: adicionar paginação no endpoint que busca todos os consertos registrados

--- src/main/java/br/com/gusmaomatheus/api/CarRepairApplication.java
+++ src/main/java/br/com/gusmaomatheus/api/CarRepairApplication.java
@@ -2,8 +2,10 @@ package br.com.gusmaomatheus.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class CarRepairApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/gusmaomatheus/api/controller/ConsertoController.java
+++ src/main/java/br/com/gusmaomatheus/api/controller/ConsertoController.java
@@ -9,6 +9,8 @@ import br.com.gusmaomatheus.api.repository.ConsertoRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -31,18 +33,16 @@ public class ConsertoController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ConsertoDTO>> listar() {
+    public ResponseEntity<Page<ConsertoDTO>> listar(Pageable pageable) {
 
         // TODO: melhorar esse map...
         // ? talvez adicionar alguma lib para mapear a entidade para o dto (modelmapper, mapstruct, outra...)
-        final List<ConsertoDTO> consertos = repository.findAll()
-                .stream()
+        final Page<ConsertoDTO> consertos = repository.findAll(pageable)
                 .map(conserto -> new ConsertoDTO(
                         conserto.getDataEntrada().toString(),
                         conserto.getDataSaida().toString(),
                         new MecanicoDTO(conserto.getMecanico().getNome(), conserto.getMecanico().getAnosDeExperiencia()),
-                        new VeiculoDTO(conserto.getVeiculo().getMarca(), conserto.getVeiculo().getModelo(), conserto.getVeiculo().getCor(), conserto.getVeiculo().getAno())))
-                .toList();
+                        new VeiculoDTO(conserto.getVeiculo().getMarca(), conserto.getVeiculo().getModelo(), conserto.getVeiculo().getCor(), conserto.getVeiculo().getAno())));
 
         return ResponseEntity.status(HttpStatus.OK).body(consertos);
     }
