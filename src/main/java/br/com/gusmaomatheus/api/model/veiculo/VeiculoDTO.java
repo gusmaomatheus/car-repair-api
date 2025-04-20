256fbe05556c7357392bdf90bc12dc4d20f9aee9
Matheus <mgusmao.dev@gmail.com>
2025-04-20 03:43:28 -0300

    fix(model/veiculo): corrigir (ou melhorar) validação para ano do veículo

--- src/main/java/br/com/gusmaomatheus/api/model/veiculo/VeiculoDTO.java
+++ src/main/java/br/com/gusmaomatheus/api/model/veiculo/VeiculoDTO.java
@@ -1,7 +1,7 @@
 package br.com.gusmaomatheus.api.model.veiculo;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 
 public record VeiculoDTO(
         @NotBlank(message = "O campo 'marca' é obrigatório.")
@@ -9,5 +9,5 @@ public record VeiculoDTO(
         @NotBlank(message = "O campo 'modelo' é obrigatório.")
         String modelo,
         @NotBlank(message = "O campo 'ano' é obrigatório.")
-        @Size(min = 4, max = 4, message = "O campo 'ano' deve possuir 4 dígitos (Ex.: 2025).")
+        @Pattern(regexp = "^\\d{4}$")
         String ano) {}
